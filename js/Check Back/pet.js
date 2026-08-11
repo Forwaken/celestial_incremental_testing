3d38bf5cfec0e142fee551979ff8294d4b7f8231
Forwaken <cbower7534@gmail.com>
2026-08-11 06:06:59 -0400

    Small fixes

--- js/Check Back/pet.js
+++ js/Check Back/pet.js
@@ -762,7 +762,7 @@ addLayer("pet", {
             player.subtabs.pu["stuff"] = "Collection"
             changeTheme()
 
-            pauseUniverseAll(["D1", "U3", "A2"], "unpause", true)
+            pauseUniverseAll(["D1", "DA", "U3", "A2"], "unpause", true)
 
             layers.pu.generateSelection();
         }
@@ -1051,7 +1051,7 @@ addLayer("pet", {
                 player.subtabs.le["stuff"] = "Shards"
                 player.subtabs.pu["stuff"] = "Selection"   
                 
-                pauseUniverseAll(["D1", "U3", "A2"], "pause", true)
+                pauseUniverseAll(["D1", "DA", "U3", "A2"], "pause", true)
             },
             style() {
                 let look = {width: '125px', minHeight: '40px', borderRadius: '0px', fontSize: '8px'}
